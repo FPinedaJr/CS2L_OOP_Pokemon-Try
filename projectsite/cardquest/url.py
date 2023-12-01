a13d22bba1140b1b93cd7d7de591c11d47ccd77a
FPinedaJr <202280110@psu.palawan.edu.ph>
2023-12-02 00:50:35 +0800

    tulugan na

--- projectsite/cardquest/url.py
+++ projectsite/cardquest/url.py
@@ -1,7 +1,10 @@
-from django.urls import path 
-from cardquest.views import HomePageView 
-from cardquest import views
+
+from django.urls import path
+from .views import HomePageView, TrainerList, PokemonCardList, CollectionList
 
 urlpatterns = [
-path('admin/', admin.site.urls), path('', views. HomePageView.as_view(), name='home'),
+    path('', HomePageView.as_view(), name='home'),
+    path('trainer_list', TrainerList.as_view(), name='trainer_list'),
+    path('pokemon-card', PokemonCardList.as_view(), name='pokemon-card'),
+    path('collection', CollectionList.as_view(), name='collection'),
 ]
--- projectsite/cardquest/views.py
+++ projectsite/cardquest/views.py
@@ -1,11 +1,34 @@
-from django.views.generic.list import ListView 
-from cardquest.models import PokemonCard
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import PokemonCard, Trainer, Collection
 
-class HomePageView (ListView):
+
+class HomePageView(ListView):
     model = PokemonCard
-    context_object_name = 'home' 
+    context_object_name = 'home'
     template_name = "home.html"
-    
-    def get_context_data(self, **kwargs): 
-        context = super().get_context_data(**kwargs) 
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         return context
+    
+    
+class TrainerList(ListView):
+    model = Trainer
+    context_object_name = 'trainer'
+    template_name = 'trainers.html'
+    paginate_by = 15
+    
+    
+class PokemonCardList(ListView):
+    model = PokemonCard
+    context_object_name = 'pokemon-card'
+    template_name = 'pokemon-card.html'
+    paginate_by = 15
+    
+    
+class CollectionList(ListView):
+    model = Collection
+    context_object_name = 'collection'
+    template_name = 'collection.html'
+    paginate_by = 15    
